26f8003db766e68bb20e28f075c4413be5b9d1e5
ali alaei <ali@ali.ali>
2020-01-12 00:46:54 -0800

    priority handling using mutex

--- myModule.c
+++ myModule.c
@@ -16,7 +16,7 @@
 #include<linux/time.h>
 
 static DEFINE_MUTEX(char_mutex); 
-//static DEFINE_MUTEX(conf_mutex); 
+static DEFINE_MUTEX(flag_mutex); 
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("ali alaei");
@@ -41,11 +41,13 @@ static struct class*  charClass;
 static int device_open(struct inode *inode, struct file *filp);
 static int device_release(struct inode *inode, struct file *filp);
 static ssize_t device_write(struct file *filp, const char *buf, size_t len, loff_t *off);
+static ssize_t device_read(struct file *filp, char *buffer, size_t length,  loff_t *offset);
 
 static struct file_operations fops = {
   .write = device_write,
   .open = device_open,
-  .release = device_release
+  .release = device_release,
+  .read = device_read,
 };
 
 struct sk_buff *sock_buff; //socket buffer
@@ -54,6 +56,7 @@ struct udphdr *udph; // udp header
 //struct tcphdr *tcph; //tcp header
 char ip_address[16]; // ip address
 u16 portNum;  //port number
+int flag=0;
 int allow=2; // 0>black list /1>white list
 char ips[100][16]; // ips which we are intrested in!
 u16 ports[100];  //ports  which we are intrested in!
@@ -110,6 +113,7 @@ static int __init lkm_example_init(void) {
   if(ret)
       printk(KERN_INFO "FAILED");
   mutex_init(&char_mutex); 
+  mutex_init(&flag_mutex);
 
   return  ret;
 }
@@ -123,15 +127,19 @@ static void __exit lkm_example_exit(void){
   nf_unregister_net_hook(&init_net,&packet_drop);
   printk(KERN_INFO"%s:Goodby OS class\n",name);
   mutex_destroy(&char_mutex); 
+  mutex_destroy(&flag_mutex);
 }
 
 //this method is invoked when the device file is opened in the application
 static int device_open(struct inode *inode, struct file *filp)
   {
+    /*
     if(!mutex_trylock(&char_mutex)){
       printk(KERN_ALERT "%s: Device in use by another process",name);
       return -EBUSY;
     }
+    */
+
     Device_Open++;
     printk(KERN_INFO"%s:device %s is opened\n", DEVICE_NAME,name);
     return 0;
@@ -142,7 +150,7 @@ static int device_open(struct inode *inode, struct file *filp)
 static int device_release(struct inode *inode, struct file *filp)
 {
   Device_Open--;
-  mutex_unlock(&char_mutex); 
+  //mutex_unlock(&char_mutex); 
   printk(KERN_INFO"%s:device %s is closed\n", name,DEVICE_NAME);
   return 0;
 }
@@ -167,10 +175,9 @@ static ssize_t device_write(struct file *filp, const char *user_buffer, size_t l
   //mutex_lock(&conf_mutex);
   printk(KERN_ALERT "%s:Writing to the device %s.\n",name, DEVICE_NAME);
   //
-  
+  mutex_lock(&char_mutex); 
   copy_from_user(kernel_buffer, user_buffer,len);
   buff_len = len;
-  copy_from_user(kernel_buffer, user_buffer,len);
   //printk(KERN_INFO "config file:\n%s\n",kernel_buffer);
   numOfFilter=0;  
   if(kernel_buffer[0]=='1')
@@ -183,30 +190,51 @@ static ssize_t device_write(struct file *filp, const char *user_buffer, size_t l
     allow=2;
     return buff_len;
   }
+  mutex_unlock(&char_mutex); 
+  // now we know type of file!
+  if(allow==1)
+  {
+    while(flag); // to fifo white list after black list!!
+    //or to ignore white lists meanwhile black list:
+    //return len;
+  }else{
+    mutex_lock(&flag_mutex); 
+    flag=1;
+    mutex_unlock(&flag_mutex); 
+  }
+
+  mutex_lock(&char_mutex); 
+  if(allow==0)
+    {
+      mutex_lock(&flag_mutex); 
+      flag=0;
+      mutex_unlock(&flag_mutex); 
+    }
   
   for(i=2;kernel_buffer[i]!='\n';i++)
   {
     if(kernel_buffer[i]=='#')
       numOfFilter++;
   }
-    offset=i+1;
-    for(l=0;l<numOfFilter;l++)
+  offset=i+1;
+  for(l=0;l<numOfFilter;l++)
+  {
+    for(i=0;kernel_buffer[i+offset]!=':';i++)
     {
-      for(i=0;kernel_buffer[i+offset]!=':';i++)
-      {
-        
-          ips[l][i]=kernel_buffer[i+offset];
-      }
-      ips[l][i]=0;
-      offset+=i+1;
-      for(i=0;kernel_buffer[i+offset]!='\n';i++)
-      {
-          tempPine[i]=kernel_buffer[i+offset];
-      }
-      tempPine[i]=0;
-      ports[l]=atoi(tempPine);
-      offset+=i+1;
+      
+        ips[l][i]=kernel_buffer[i+offset];
     }
+    ips[l][i]=0;
+    offset+=i+1;
+    for(i=0;kernel_buffer[i+offset]!='\n';i++)
+    {
+        tempPine[i]=kernel_buffer[i+offset];
+    }
+    tempPine[i]=0;
+    ports[l]=atoi(tempPine);
+    offset+=i+1;
+  }//end of critical section
+  mutex_unlock(&char_mutex); 
   printk(KERN_INFO "%s:status : %d\n",name,allow);
   for (i=0;i<numOfFilter;i++)
   {
@@ -214,6 +242,22 @@ static ssize_t device_write(struct file *filp, const char *user_buffer, size_t l
   }
   return len;
 }
+static ssize_t device_read(struct file *filp, /* see include/linux/fs.h   */
+                           char *user_buffer,      /* buffer to fill with data */
+                           size_t length,     /* length of the buffer     */
+                           loff_t *offset)
+{
+  printk(KERN_ALERT "Reading from the device %s.\n", DEVICE_NAME);
+
+  if (length<=buff_len){
+    copy_to_user(user_buffer, kernel_buffer, length);
+    return length;
+  }else{
+    copy_to_user(user_buffer, kernel_buffer, buff_len);
+    return buff_len;
+  }
+
+ }
 
 u8 equlAddresses(char * ip1,u16 port1,char * ip2,u16 port2)
 {
